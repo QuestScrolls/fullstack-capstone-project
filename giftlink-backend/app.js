4e6a4484290235161b3d7ef56070bb39bfe30068
QuestScrolls <mixiyomi@gamil.com>
2025-12-30 23:33:15 -0500

    Login Route integration

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -28,6 +28,9 @@ const giftRoutes = require('./routes/giftRoutes');
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 const searchRoutes = require('./routes/searchRoutes');
 
+// Register API import
+const authRoutes = require('./routes/authRoutes');
+
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
@@ -40,6 +43,9 @@ app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 app.use('/api/search', searchRoutes);
+
+// Register API use
+app.use('/api/auth', authRoutes);
 
 
 // Global Error Handler
